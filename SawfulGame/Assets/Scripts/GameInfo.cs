6dc36b83d59f9aa250e6bfc929c4c39f8355b982
nac5961 <nac5961@g.rit.edu>
2019-03-25 10:52:17 -0400

    Started working on the spawning logic.

---
+++ SawfulGame/Assets/Scripts/GameInfo.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Script to keep track of various game information required between multiple scenes
+/// </summary>
+public class GameInfo : MonoBehaviour
+{
+
+    //Represents the game info that is stored across all scenes
+    public static GameInfo instance;
+
+    public float moveSpeed;
+    public float spawnRate;
+    public int maxCombination;
+
+    private bool gameStart = false;
+    private bool gameOver = false;
+    private bool paused = false;
+
+    private float initialMoveSpeed;
+    private float initialSpawnRate;
+    private int initialCombination;
+
+    public bool GameStart
+    {
+        get { return gameStart; }
+        set { gameStart = value; }
+    }
+
+    public bool GameOver
+    {
+        get { return gameOver; }
+        set { gameOver = value; }
+    }
+
+    public bool Paused
+    {
+        get { return paused; }
+        set { paused = value; }
+    }
+
+    public float MoveSpeed
+    {
+        get { return moveSpeed; }
+        set { moveSpeed = value; }
+    }
+
+    public float SpawnRate
+    {
+        get { return spawnRate; }
+        set { spawnRate = value; }
+    }
+
+    public int MaxCombination
+    {
+        get { return maxCombination; }
+        set { maxCombination = value; }
+    }
+
+    void Awake()
+    {
+        //If there is not already a GameInfo object, set it to this
+        if (instance == null)
+        {
+            //Make sure we keep running when clicking off the screen
+            Application.runInBackground = true;
+
+            //Object this is attached to will be preserved between scenes
+            DontDestroyOnLoad(gameObject);
+
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            //Ensures that there are no duplicate objects being made every time the scene is loaded
+            Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        gameStart = instance.gameStart;
+        gameOver = instance.gameOver;
+        paused = instance.paused;
+        moveSpeed = instance.moveSpeed;
+        spawnRate = instance.spawnRate;
+        maxCombination = instance.maxCombination;
+        initialMoveSpeed = moveSpeed;
+        initialSpawnRate = spawnRate;
+        initialCombination = maxCombination;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Temporary method to reload the scene.
+    /// We probably won't use this for the final build.
+    /// </summary>
+    public void ReloadMainMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+
+    public void ResetDistance()
+    {
+        moveSpeed = initialMoveSpeed;
+        spawnRate = initialSpawnRate;
+    }
+}
---
+++ SawfulGame/Assets/Scripts/Spawning.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawning : MonoBehaviour
+{
+    public GameObject platforms;
+
+    private float timer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = GameInfo.instance.SpawnRate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Spawns platforms at the given interval
+    /// </summary>
+    private void SpawnPlatforms()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            timer = GameInfo.instance.SpawnRate;
+
+            //Generate letter or combination
+
+            //Generate 3 platforms
+
+            //Assign each platform a letter or combination
+            
+        }
+    }
+
+    /// <summary>
+    /// Generates a combination associated with each platform
+    /// </summary>
+    private void GenerateCombination()
+    {
+
+    }
+}
