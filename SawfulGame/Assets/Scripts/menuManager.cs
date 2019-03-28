7ee8a4f9dfc6d4ff9365e63a04e4f42fdaab6355
Max Kaiser <MaxRKaiser18@gmail.com>
2019-03-28 16:13:59 -0400

    Main Menu stuff (first draft). Still needs info in how to play, logo art, general cleanup

---
+++ SawfulGame/Assets/Scripts/menuManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class menuManager : MonoBehaviour
+{
+
+    public GameObject howToPanel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void loadGame()
+    {
+        SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
+    }
+
+    public void loadMenu()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+
+    public void toggleHowTo()
+    {
+        if (!howToPanel.activeInHierarchy)
+        {
+            howToPanel.SetActive(true);
+        }
+        else
+        {
+            howToPanel.SetActive(false);
+        }
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
