cedd08eb67818760dc77f293ac4f5648ced19d43
Max Kaiser <MaxRKaiser18@gmail.com>
2019-04-04 18:59:48 -0400

    Added game over panel and score

--- SawfulGame/Assets/Scripts/menuManager.cs
+++ SawfulGame/Assets/Scripts/menuManager.cs
@@ -6,24 +6,34 @@ using TMPro;
 
 public class menuManager : MonoBehaviour
 {
-
+    public int currentScene; //0 - main menu, 1 - main scene
     public GameObject howToPanel;
-
+    public GameObject gameOverPanel;
+    public float score;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI scoreText2;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentScene == 1)
+        {
+            //score++; [DEBUG LEFTOVER]
+            //score = GameInfo.instance. [something here];
+            scoreText2.SetText("Score: {0}", score);
+        }
+
     }
 
     public void loadGame()
     {
+        //GameInfo.instance.GameOver = false;
         SceneManager.LoadScene("TestSpawningScene", LoadSceneMode.Single);
     }
 
@@ -42,6 +52,20 @@ public class menuManager : MonoBehaviour
         {
             howToPanel.SetActive(false);
         }
+    }
+
+    public void toggleGameOver()
+    {
+        if (!gameOverPanel.activeInHierarchy)
+        {
+            gameOverPanel.SetActive(true);
+        }
+        else
+        {
+            gameOverPanel.SetActive(false);
+        }
+
+        scoreText.SetText("Final Score: {0}", score);
     }
 
     public void exitGame()
